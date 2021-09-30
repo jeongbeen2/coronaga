1c09e062cf0bef6dc58ce821007b43940a88482a
jeongbeen2 <aceman9508@gmail.com>
2021-09-30 12:29:42 +0900

    [Feat] Vaccine API 추가

--- src/api/vacccineData.ts
+++ src/api/vacccineData.ts
@@ -1,2 +1,4 @@
 import axios from "axios";
-export function vaccine(search: string) {}
+export function vaccine(search: string) {
+  const vaccineApi = `https://infuser.odcloud.kr/oas/docs?namespace=15077756​/v1​/`;
+}
