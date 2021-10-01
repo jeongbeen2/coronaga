8e02a8d95448704efe53fec12c327e996de38062
jeongbeen2 <aceman9508@gmail.com>
2021-10-01 22:04:47 +0900

    [Feat] vaccine api get axios

--- src/api/vacccineData.ts
+++ src/api/vacccineData.ts
@@ -1,4 +1,12 @@
 import axios from "axios";
 export function vaccine(search: string) {
   const vaccineApi = `https://infuser.odcloud.kr/oas/docs?namespace=15077756​/v1​/`;
+  return axios
+    .get(vaccineApi)
+    .then((resp) => resp.data)
+    .then((data) => {
+      return new Promise((resolve) => {
+        return setTimeout(() => resolve(data), 1000);
+      });
+    });
 }
